08aa23673f56766ef7f161506c138e309135e01a
nir0s <nir36g@gmail.com>
2016-01-20 21:00:20 +0200

    removed unnecessary constants and made globals non-public

--- ld/constants.py
+++ ld/constants.py
@@ -1,20 +1,7 @@
-UNIXCONFDIR = '/etc'
-LSB_RELEASE = '/etc/lsb-release'
-OS_RELEASE = '/etc/os-release'
-ARCH_RELEASE = '/etc/arch-release'
-ORACLE_RELEASE = '/etc/oracle-release'
-ENTERPRISE_RELEASE = '/etc/enterprise-release'
-REDHAT_RELEASE = '/etc/redhat-release'
-CENTOS_RELEASE = '/etc/centos-release'
-DEBIAN_VERSION = '/etc/debian_version'
-FEDORA_RELEASE = '/etc/fedora-release'
-SUSE_RELEASE = '/etc/SuSE-release'
-SLACKWARE_VERSION = '/etc/slackware-version'
-EXHERBO_RELEASE = '/etc/exherbo-release'
-PARALLELS_RELEASE = '/etc/parallels-release'
+_UNIXCONFDIR = '/etc'
+_OS_RELEASE = '/etc/os-release'
 
-
-DIST_TO_BASE = {
+_DIST_TO_BASE = {
     'debian': 'debian',
     'ubuntu': 'debian',
     'linuxmint': 'debian',
