42c9bd20b59d0bcaab46505c4ff907d33c4cb2d9
Andreas Maier <maiera@de.ibm.com>
2016-03-13 08:38:32 +0100

    Added ibmkvm to set of reliable distros.

--- ld/__init__.py
+++ ld/__init__.py
@@ -716,6 +716,7 @@ class LinuxDistribution(object):
         "exherbo"       Exherbo Linux
         "gentoo"        GenToo Linux
         "ibm_powerkvm"  IBM PowerKVM
+        "ibmkvm"        KVM for IBM z Systems
         "linuxmint"     Linux Mint
         "mageia"        Mageia
         "mandriva"      Mandriva Linux
