d2f10c40d2059a1c795e813e72bd9b400a17f0bc
Samuel FORESTIER <dev@samuel.domains>
2021-01-10 11:02:42 +0100

    Re-adds an empty new-line that somehow got lost in `distro.py`

--- distro.py
+++ distro.py
@@ -37,6 +37,7 @@ import logging
 import argparse
 import subprocess
 
+
 _UNIXCONFDIR = os.environ.get('UNIXCONFDIR', '/etc')
 _UNIXUSRLIBDIR = os.environ.get('UNIXUSRLIBDIR', '/usr/lib')
 _OS_RELEASE_BASENAME = 'os-release'
