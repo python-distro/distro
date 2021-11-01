abb3feecbaaa4556b000137eadc3abed4632d4f2
Samuel FORESTIER <dev@samuel.domains>
2021-11-01 11:04:04 +0100

    Removes `subprocess.CalledProcessError` from possible exceptions
    `subprocess.CalledProcessError` exceptions are now caught since 9260b7bd

--- src/distro/distro.py
+++ src/distro/distro.py
@@ -719,10 +719,6 @@ class LinuxDistribution:
         * :py:exc:`OSError`: Some I/O issue with an os-release file or distro
           release file.
 
-        * :py:exc:`subprocess.CalledProcessError`: The lsb_release command had
-          some issue (other than not being available in the program execution
-          path).
-
         * :py:exc:`UnicodeError`: A data source has unexpected characters or
           uses an unexpected encoding.
         """
