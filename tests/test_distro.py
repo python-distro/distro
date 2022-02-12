cd6ab22dbe83556fe26a7ec0373850eb858bfab3
Samuel FORESTIER <dev@samuel.domains>
2022-02-12 13:51:43 +0100

    Restores compatibility shim with `platform.linux_distribution`
    distro#230 introduced proper support for os-release `VERSION_CODENAME`
    attribute but broke compatibility shim with (now defunct)
    `platform.linux_distribution` as the third tuple member (codename) were
    not taken from `VERSION` os-release attribute anymore.
    
    Closes #238.

--- tests/test_distro.py
+++ tests/test_distro.py
@@ -1833,6 +1833,9 @@ class TestInfo(DistroTestCase):
         self.ubuntu14_os_release = os.path.join(
             DISTROS_DIR, "ubuntu14", "etc", "os-release"
         )
+        self.fedora30_os_release = os.path.join(
+            DISTROS_DIR, "fedora30", "etc", "os-release"
+        )
 
     def test_info(self):
         _distro = distro.LinuxDistribution(
@@ -1906,6 +1909,12 @@ class TestInfo(DistroTestCase):
         assert i == ("Ubuntu", "14.04", "Trusty Tahr")
 
     def test_linux_distribution_full_false(self):
+        _distro = distro.LinuxDistribution(
+            include_lsb=False, os_release_file=self.fedora30_os_release
+        )
+        i = _distro.linux_distribution()
+        assert i == ("Fedora", "30", "Thirty")
+
         _distro = distro.LinuxDistribution(
             include_lsb=False, os_release_file=self.ubuntu14_os_release
         )
